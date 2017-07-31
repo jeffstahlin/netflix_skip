80787e202e0ae33f735d62e2f33863e62459fc71
jeffstahlin <jeffstahlin@gmail.com>
2017-07-31 22:56:54 +0200

    init

---
+++ js/options.js
@@ -0,0 +1,5 @@
+var main_tabs = new Tabs('#main_tabs');
+
+document.addEventListener('optionsPageReady', function () {
+    /* Options page is ready. Write your code here */
+});
---
+++ js/storage.js
@@ -0,0 +1,8 @@
+var storage = {
+    // `sync` if you want. remember about QUOTAS https://developer.chrome.com/extensions/storage.html#sync-properties
+    area: chrome.storage.local,
+    default_options: {
+        // one key for every option
+        enabled: 1
+    }
+};
